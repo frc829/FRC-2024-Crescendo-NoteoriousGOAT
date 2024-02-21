a5e54b7820f18cfe170144b79eace1fc28756246
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-21 12:03:11 -0500

    updates

--- src/main/java/com/compLevel0/LEDController.java
+++ src/main/java/com/compLevel0/LEDController.java
@@ -1,5 +1,23 @@
 package com.compLevel0;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 public class LEDController {
-    
+
+    public final Supplier<Optional<String>> currentAnimation;
+    public final Supplier<Optional<String>> currentColor;
+
+    public final Runnable update;
+
+    private LEDController(
+            Supplier<Optional<String>> currentAnimation,
+            Supplier<Optional<String>> currentColor,
+            Runnable update) {
+        this.currentAnimation = currentAnimation;
+        this.currentColor = currentColor;
+        this.update = update;
+
+    }
+
 }
