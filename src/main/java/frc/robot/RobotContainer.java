ab7351ed83bd15b24bcbfb8b8d23939a1bba5ded
Nick Armstrong <narmstro@warren.k12.in.us>
2024-03-16 12:25:37 -0400

    updates

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commandCreators.AdvancedScoringCommands;
 import frc.robot.commandCreators.BasicCommands;
 import frc.robot.commandCreators.DriveCommands;
@@ -106,7 +107,7 @@ public class RobotContainer {
         public static final PowerDistribution pdh = new PowerDistribution();
         // public static final LEDSubsystem ledSubsystem = LEDSubsystem.create.get();
 
-        private final SendableChooser<Command> autoChooser;
+        // private final SendableChooser<Command> autoChooser;
 
         public RobotContainer() {
                 SmartDashboard.putData("PDH", pdh);
@@ -181,8 +182,8 @@ public class RobotContainer {
 
                 new AutoCommands();
 
-                autoChooser = AutoBuilder.buildAutoChooser();
-                SmartDashboard.putData("Auto Chooser", autoChooser);
+                // autoChooser = AutoBuilder.buildAutoChooser();
+                // SmartDashboard.putData("Auto Chooser", autoChooser);
 
         }
 
@@ -213,6 +214,6 @@ public class RobotContainer {
         }
 
         public Command getAutonomousCommand() {
-                return autoChooser.getSelected();
+                return Commands.none();
         }
 }
